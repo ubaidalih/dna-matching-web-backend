336228f0d79269cc2913897cb99877815d598d0e
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-04-22 21:45:36 +0700

    feat : backend search history 50%

--- src/main.go
+++ src/main.go
@@ -10,6 +10,21 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type hasilPrediksi struct {
+	Id         int    `json:"id"`
+	Nama       string `json:"nama"`
+	Tanggal    string `json:"tanggal"`
+	Penyakit   string `json:"penyakit"`
+	Status     string `json:"status"`
+	Presentase int    `json:"presentase"`
+}
+
+type penyakit struct {
+	Id       int    `json:"id"`
+	Penyakit string `json:"penyakit"`
+	DNA      string `json:"dna"`
+}
+
 func main() {
 	// Connect to Postgres
 	connStr := "user=cnvpeuhhrqssab dbname=dcfnvjg2a29rfj password=ff5dd6b6ee20aff980ce149bccd0a782ee6dbb13347aa5fc431b86d4cae60c5d host=ec2-3-217-251-77.compute-1.amazonaws.com port=5432"
@@ -33,6 +48,30 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/result", func(c echo.Context) error {
+		//validate search query
+		//parse search query
+		//benerin format tanggal
+		var hasil_prediksi []hasilPrediksi
+		rows, err := db.Query("SELECT * FROM hasil_prediksi")
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+		for rows.Next() {
+			hasil := new(hasilPrediksi)
+			err := rows.Scan(&hasil.Id, &hasil.Nama, &hasil.Tanggal, &hasil.Penyakit, &hasil.Status, &hasil.Presentase)
+			if err != nil {
+				return err
+			}
+			hasil_prediksi = append(hasil_prediksi, *hasil)
+		}
+		return c.JSON(http.StatusOK, hasil_prediksi)
+	})
+	// e.POST("/result", insertQuery)
+	// e.GET("/test", testResult)
+	// e.POST("/test", insertTest)
+	// e.POST("/disease", insertDisease)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
